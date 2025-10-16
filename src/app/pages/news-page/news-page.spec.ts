7c865ceb6752a5e246927bdfad6f63a0e585e640
Vinn <vinnlauro@gmail.com>
2025-10-16 11:58:31 +0800

    create news page

---
+++ src/app/pages/news-page/news-page.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NewsPage } from './news-page';
+
+describe('NewsPage', () => {
+  let component: NewsPage;
+  let fixture: ComponentFixture<NewsPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [NewsPage],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(NewsPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/news-page/news-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-page',
+  imports: [],
+  templateUrl: './news-page.html',
+  styleUrl: './news-page.css'
+})
+export class NewsPage {
+
+}
