86106e3f1557d1f04e301e8d3a0120a46b2c51c2
Vinn <vinnlauro@gmail.com>
2025-10-16 11:51:37 +0800

    edit to test api service initialize

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -3,14 +3,16 @@ import { TestBed } from '@angular/core/testing';
 import { ApiService } from './api.service';
 
 describe('ApiService', () => {
-  let service: ApiService;
+  describe('initialization', () => {
+    let service: ApiService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiService);
-  });
+    beforeEach(() => {
+      TestBed.configureTestingModule({});
+      service = TestBed.inject(ApiService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+    it('should be created', () => {
+      expect(service).toBeDefined();
+    });
   });
 });
