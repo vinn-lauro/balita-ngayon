ebf7c8a6f67a0a0f81968a1fccd15c87a13c8e60
Vinn <vinnlauro@gmail.com>
2025-10-17 11:03:56 +0800

    edit news page

--- src/app/pages/news-page/news-page.spec.ts
+++ src/app/pages/news-page/news-page.spec.ts
@@ -3,20 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NewsPage } from './news-page';
 
 describe('NewsPage', () => {
-  let component: NewsPage;
-  let fixture: ComponentFixture<NewsPage>;
+  describe('initialization', () => {
+    let component: NewsPage;
+    let fixture: ComponentFixture<NewsPage>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [NewsPage],
-    }).compileComponents();
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        imports: [NewsPage],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(NewsPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      fixture = TestBed.createComponent(NewsPage);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
   });
 });
