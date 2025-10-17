483cd52cfb64ad1d4812b94cb530f2c7252f3d40
Vinn <vinnlauro@gmail.com>
2025-10-17 09:26:23 +0800

    add http service testing

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,18 +1,53 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ApiService } from './api.service';
+import { MOCK_NEWS } from '../testing/MOCK_NEWS';
+import { HttpClient, HttpErrorResponse, provideHttpClient } from '@angular/common/http';
+import { asyncData } from '../testing/helper/asyncData';
+import { throwError } from 'rxjs';
 
 describe('ApiService', () => {
   describe('initialization', () => {
     let service: ApiService;
 
     beforeEach(() => {
-      TestBed.configureTestingModule({});
+      TestBed.configureTestingModule({
+        providers: [provideHttpClient()],
+      });
       service = TestBed.inject(ApiService);
     });
 
     it('should be created', () => {
       expect(service).toBeDefined();
+    });
+  });
+
+  describe('HttpClient (with spies)', () => {
+    let apiService: ApiService;
+    let httpClientSpy: jasmine.SpyObj<HttpClient>;
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers: [ApiService, provideHttpClient()],
+      });
+
+      httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+      apiService = new ApiService(httpClientSpy);
+    });
+
+    it('should return expected news (HttpClient called once)', (done: DoneFn) => {
+      const expectedNews = MOCK_NEWS;
+
+      httpClientSpy.get.and.returnValue(asyncData(expectedNews));
+
+      apiService.getNews().subscribe({
+        next: (news) => {
+          expect(news).withContext('expected news').toEqual(expectedNews);
+          done();
+        },
+        error: done.fail,
+      });
+      expect(httpClientSpy.get.calls.count()).withContext('one call').toBe(1);
     });
   });
 });
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-  
+  private readonly apiUrl: string = environment.api.url;
+  private readonly apiKey: string = environment.api.key;
+
+  constructor(private readonly http: HttpClient) {}
+
+  public getNews(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}?access_key=${this.apiKey}`);
+  }
 }
