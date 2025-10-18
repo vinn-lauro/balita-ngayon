b9d1a849e542c3e6a6d2c5f5450c87bcc4b95b1a
Vinn <vinnlauro@gmail.com>
2025-10-18 10:50:25 +0800

    add local storage service tests

--- src/app/services/local-storage.service.spec.ts
+++ src/app/services/local-storage.service.spec.ts
@@ -1,16 +1,92 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
 import { LocalStorageService } from './local-storage.service';
+import { PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { MOCK_LOCAL_STORAGE } from '../testing/MOCK_LOCAL_STORAGE';
+import { MOCK_NEWS } from '../testing/MOCK_NEWS';
 
 describe('LocalStorageService', () => {
-  let service: LocalStorageService;
+  describe('initialization', () => {
+    let service: LocalStorageService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(LocalStorageService);
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers: [
+          {
+            provide: PLATFORM_ID,
+            useValue: 'browser',
+          },
+        ],
+      });
+      service = TestBed.inject(LocalStorageService);
+    });
+
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
+
+    it('should correctly identify the platform as browser', inject(
+      [PLATFORM_ID],
+      (platformId: Object) => {
+        expect(isPlatformBrowser(platformId)).toBe(true);
+        expect(isPlatformServer(platformId)).toBe(false);
+      }
+    ));
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('test with mock data', () => {
+    let localStorageMock = MOCK_LOCAL_STORAGE;
+    let localStorageService: LocalStorageService;
+    let setItemSpy: jasmine.Spy;
+    let getItemSpy: jasmine.Spy;
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({});
+      localStorageService = TestBed.inject(LocalStorageService);
+      setItemSpy = spyOn(localStorageService, 'setItem').and.callFake(localStorageMock.setItem);
+      getItemSpy = spyOn(localStorageService, 'getItem').and.callFake(localStorageMock.getItem);
+      localStorageMock.clear();
+    });
+
+    it('should call localStorage.setItem when saving data', () => {
+      const news = MOCK_NEWS;
+
+      localStorageService.setItem('news', news);
+
+      expect(setItemSpy).toHaveBeenCalledTimes(1);
+      expect(setItemSpy).toHaveBeenCalledWith('news', MOCK_NEWS);
+    });
+
+    it('should call localStorage.getItem and return stored data', () => {
+      const expectedNews = MOCK_NEWS;
+
+      localStorageMock.setItem('news', expectedNews);
+      const result = localStorageService.getItem('news');
+
+      expect(getItemSpy).toHaveBeenCalledTimes(1);
+      expect(getItemSpy).toHaveBeenCalledWith('news');
+      expect(result).toEqual(expectedNews);
+    });
+
+    it('should remove data from local storage', () => {
+      const key = 'news';
+      const expectedNews = MOCK_NEWS;
+
+      localStorageMock.setItem(key, expectedNews);
+      localStorageMock.removeItem(key);
+
+      expect(localStorageMock.getItem(key)).toBeNull();
+    });
+
+    it('should clear data from local storage', () => {
+      const key = 'news';
+      const expectedNews = MOCK_NEWS;
+
+      localStorageMock.setItem(key, expectedNews);
+      localStorageMock.clear();
+
+      expect(localStorageMock.getItem(key)).toBeNull();
+    });
   });
 });
--- src/app/services/local-storage.service.ts
+++ src/app/services/local-storage.service.ts
@@ -1,8 +1,51 @@
-import { Injectable } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LocalStorageService {
-  
+  private isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
+
+  public setItem(key: string, value: any): void {
+    if (this.isBrowser) {
+      try {
+        const valueToStore = JSON.stringify(value);
+        localStorage.setItem(key, valueToStore);
+      } catch (e: unknown) {
+        console.error('Error saving to local storage', e);
+      }
+    }
+  }
+
+  public getItem<T>(key: string): T | null {
+    if (this.isBrowser) {
+      const item = localStorage.getItem(key);
+      if (!item) {
+        return null;
+      }
+      try {
+        return JSON.parse(item) as T;
+      } catch (e: unknown) {
+        return item as unknown as T;
+      }
+    }
+    return null;
+  }
+
+  public removeItem(key: string): void {
+    if (this.isBrowser) {
+      localStorage.removeItem(key);
+    }
+  }
+
+  public clear(): void {
+    if (this.isBrowser) {
+      localStorage.clear();
+    }
+  }
 }
