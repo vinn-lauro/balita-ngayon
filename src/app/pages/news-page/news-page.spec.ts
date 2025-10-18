180fc5f74c6f7741c8e57d9451e9e6d06e94267f
Vinn <vinnlauro@gmail.com>
2025-10-18 09:11:10 +0800

    add initialize component test

--- src/app/pages/news-page/news-page.spec.ts
+++ src/app/pages/news-page/news-page.spec.ts
@@ -1,24 +1,51 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
 
 import { NewsPage } from './news-page';
+import { ApiService } from '../../services/api.service';
+import { provideHttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+import { MOCK_NEWS } from '../../testing/MOCK_NEWS';
 
 describe('NewsPage', () => {
   describe('initialization', () => {
     let component: NewsPage;
     let fixture: ComponentFixture<NewsPage>;
+    let apiService: ApiService;
+    let componentApiService: ApiService;
 
     beforeEach(async () => {
       await TestBed.configureTestingModule({
         imports: [NewsPage],
+        providers: [ApiService, provideHttpClient()],
       }).compileComponents();
 
       fixture = TestBed.createComponent(NewsPage);
       component = fixture.componentInstance;
       fixture.detectChanges();
+
+      apiService = fixture.debugElement.injector.get(ApiService);
+      componentApiService = apiService;
+      apiService = TestBed.inject(ApiService);
     });
 
     it('should create', () => {
       expect(component).toBeTruthy();
+    });
+
+    it("should inject the component's ApiService instance", inject(
+      [ApiService],
+      (service: ApiService) => {
+        expect(service).toBe(componentApiService);
+      }
+    ));
+
+    it('TestBed and Component UserService should be the same', () => {
+      expect(apiService).toBe(componentApiService);
+    });
+
+    it('should initialize news Observable in ngOnInit', async () => {
+      await fixture.isStable();
+      expect(component.new$).toBeDefined();
     });
   });
 });
--- src/app/pages/news-page/news-page.ts
+++ src/app/pages/news-page/news-page.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { NewsModel } from '../../models/news.model';
+import { ApiService } from '../../services/api.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-news-page',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './news-page.html',
-  styleUrl: './news-page.css'
+  styleUrl: './news-page.css',
 })
-export class NewsPage {
+export class NewsPage implements OnInit {
+  private newsOb$: Observable<NewsModel | null>;
+  private apiService = inject(ApiService);
 
+  constructor() {
+    this.newsOb$ = of(null);
+  }
+
+  public ngOnInit(): void {
+    this.onGetNews();
+  }
+
+  public get new$(): Observable<NewsModel | null> {
+    return this.newsOb$;
+  }
+
+  private onGetNews(): void {
+    this.newsOb$ = this.apiService.getNews();
+  }
 }
